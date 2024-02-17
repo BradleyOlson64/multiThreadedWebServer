377e018b6bb7b73d9048e853282ebf68cd89cffb
BradleyOlson64 <lotrftw9@gmail.com>
2024-02-16 18:24:55 -0800

    Create listener and handle connections

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # multiThreadedWebServer
 
 This is a server based on the Rust Book final project. The intent with this project is to practice using web protocols (TCP, HTTP) as well as async Rust.
+
+I add in error handling to make the example a bit more realistic.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::io::{prelude::*, BufReader};
+use std::net::{TcpListener, TcpStream};
 fn main() {
-    println!("Hello, world!");
+    let outcome = open_connections().err();
+    match outcome {
+        Some(err) => println!("{:?}", err),
+        None => ()
+    }
+}
+
+fn open_connections() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:7878")?;
+    for stream in listener.incoming() {
+        let stream = stream?;
+        handle_connection(stream)?;
+    }
+    Ok(())
+}
+
+fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+    let buf_reader = BufReader::new(&mut stream);
+    let maybe_lines = buf_reader.lines();
+    let mut http_request: Vec<_> = vec![];
+    for maybe_line in maybe_lines {
+        let line = maybe_line?;
+        if !line.is_empty() {
+            http_request.push(line);
+        } else {
+            break;
+        }
+    }
+
+    println!("Request: {:#?}", http_request);
+    Ok(())
 }
