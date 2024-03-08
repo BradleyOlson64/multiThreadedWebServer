d7dc354f9050e4d9792d906edd92335183b22851
BradleyOlson64 <lotrftw9@gmail.com>
2024-03-07 16:50:17 -0800

    Built out single threaded version further

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 use std::io::{prelude::*, BufReader};
 use std::net::{TcpListener, TcpStream};
+use std::fs;
 fn main() {
-    let outcome = open_connections().err();
-    match outcome {
+    match open_connections().err() {
         Some(err) => println!("{:?}", err),
         None => ()
     }
 }
 
-fn open_connections() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:7878")?;
+fn open_connections() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878")
+        .map_err(|err| std::io::Error::new(std::io::ErrorKind::NotFound, err))?;
     for stream in listener.incoming() {
         let stream = stream?;
         handle_connection(stream)?;
@@ -17,7 +18,7 @@ fn open_connections() -> Result<(), std::io::Error> {
     Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
+fn handle_connection(mut stream: TcpStream) -> std::io::Result<()> {
     let buf_reader = BufReader::new(&mut stream);
     let maybe_lines = buf_reader.lines();
     let mut http_request: Vec<_> = vec![];
@@ -29,6 +30,19 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), std::io::Error> {
             break;
         }
     }
+    let request_line = http_request[0].clone();
+
+    if request_line == "GET / HTTP/1.1" {
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("hello.html")?;
+        let len = contents.len();
+        let response = format!("{status_line}\r\nContent-Length: {len}\r\n\r\n{contents}");
+
+        stream.write_all(response.as_bytes())?;
+    } else {
+        //Do nothing
+    }
+    
 
     println!("Request: {:#?}", http_request);
     Ok(())
